122f5ebb96132ef09e8aeba14d7dd14326dfb8cf
Anna Morawska <ania.morawska92@gmail.com>
2018-12-19 20:41:12 +0100

    Remove unnecessary cd command from build process

--- src/helpers/build.js
+++ src/helpers/build.js
@@ -1,14 +1,10 @@
 const { cd, exec } = require('shelljs');
 
 const build = repoDir => {
-
-    cd(repoDir);
-    exec('npm install');
-    exec('npm run build', { silent: true });
-    exec('npm pack');
-    cd('..');
-
-
+  cd(repoDir);
+  exec('npm install');
+  exec('npm run build', { silent: true });
+  exec('npm pack');
 };
 
 module.exports = build;
