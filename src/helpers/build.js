65f3f8f80631fd9a10350c33d8dd4f5a26d535ca
Anna Morawska <a.morawska@mdbootstrap.com>
2018-12-20 13:59:31 +0100

    Add ternary expression to decide if npm pack process should start

--- src/helpers/build.js
+++ src/helpers/build.js
@@ -1,10 +1,10 @@
 const { cd, exec } = require('shelljs');
 
-const build = repoDir => {
+const build = (repoDir, pack = false) => {
   cd(repoDir);
   exec('npm install');
   exec('npm run build', { silent: true });
-  exec('npm pack');
+  pack && exec('npm pack');
 };
 
 module.exports = build;
