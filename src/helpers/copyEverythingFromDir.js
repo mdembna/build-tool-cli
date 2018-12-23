5da96f3e303f3cc8416fc7a3ca4fe7c6e1039274
Anna Morawska <a.morawska@mdbootstrap.com>
2018-12-23 10:50:45 +0100

    Add copyEverythingFromDir helper function

---
+++ src/helpers/copyEverythingFromDir.js
@@ -0,0 +1,22 @@
+const fs = require('fs-extra');
+const path = require('path');
+const log = require('./log');
+const { ERROR, SUCCESS } = require('../constans/log-types');
+
+const copyEverythingFromDir = (baseDirName, targetDirName) => {
+    try {
+        fs.readdirSync(baseDirName).forEach(file => {
+            const fileBasePath = path.join(baseDirName, file);
+            const fileTargetPath = path.join(targetDirName, file);
+
+            fs.copySync(fileBasePath, fileTargetPath);
+            log(`${file} copied`, SUCCESS);
+        });
+    } catch (err) {
+        log(`An error occurred while copying files`, ERROR);
+        console.log(err);
+        process.exit();
+    }
+}
+
+module.exports = copyEverythingFromDir;
