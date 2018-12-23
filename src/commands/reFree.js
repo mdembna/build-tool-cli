b893bd4f40df0e277b0192870043146653ccf6e4
Anna Morawska <a.morawska@mdbootstrap.com>
2018-12-23 10:44:22 +0100

    Add sript for react free command

---
+++ src/commands/reFree.js
@@ -0,0 +1,59 @@
+const build = require('../helpers/build');
+const { repositories } = require('../../config.json');
+const getPathToRepo = require('../helpers/getPathToRepo');
+const createZip = require('../setup/createZip');
+const searchFileByExtension = require('../helpers/searchFileByExtension');
+const deleteFiles = require('../helpers/deleteFiles');
+const path = require('path');
+const { RE_FREE } = require('../constans/packages-types');
+const replaceStringInFile = require('../helpers/replaceStringInFile');
+
+
+const checkoutToBranch = require('../setup/checkoutToBranch');
+const copyEverythingFromDir = require('../helpers/copyEverythingFromDir');
+const readAndDeleteFromFile = require('../helpers/readAndDeleteFromFile');
+const copyFiles = require('../helpers/copyFiles');
+
+
+const reFree = async ({ version, lastVersionNumber }) => {
+    const baseRepoName = 're-pro';
+    const packageRepoName = 'react-demo';
+    const workingBranch = 'free-auto';
+    const targetRepoName = 'React-Bootstrap-with-Material-Design';
+
+    const baseRepoPath = getPathToRepo(baseRepoName);
+    const targetRepoPath = getPathToRepo(targetRepoName);
+    const packageRepoPath = getPathToRepo(packageRepoName);
+
+    const dirToDelete = repositories[targetRepoName].dirToDelete;
+    const filesToEdit = repositories[targetRepoName].filesToEdit;
+    const targetRepoFilesToCopy = repositories[targetRepoName].filesToCopy;
+
+
+    checkoutToBranch(baseRepoName, workingBranch, "dev");
+    copyEverythingFromDir(baseRepoPath, targetRepoPath);
+    deleteFiles(dirToDelete, targetRepoPath);
+    await readAndDeleteFromFile(filesToEdit, targetRepoPath, baseRepoPath);
+
+    build(targetRepoPath, true);
+
+    checkoutToBranch(packageRepoName, workingBranch, "auto-version-update");
+    const lastTgzPackage = searchFileByExtension(packageRepoPath, '.tgz');
+    lastTgzPackage && deleteFiles(lastTgzPackage, packageRepoPath);
+
+
+    replaceStringInFile(
+        ["package.json"],
+        packageRepoPath,
+        `./mdbreact-${version}.tgz`,
+        `${version}`
+    );
+
+    deleteFiles(['src/pages'], packageRepoPath);
+
+    copyFiles(targetRepoFilesToCopy, path.join(targetRepoPath, "docs"), path.join(packageRepoPath, "src"));
+
+    await createZip(packageRepoName, RE_FREE);
+};
+
+module.exports = reFree;
