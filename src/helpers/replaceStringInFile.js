eefe9ea824ba9b5aa278c1fa8a35bddcf475208f
Anna Morawska <a.morawska@mdbootstrap.com>
2019-03-18 07:57:30 +0100

    Add regex matcher to be more specific when replacing the version number

--- src/helpers/replaceStringInFile.js
+++ src/helpers/replaceStringInFile.js
@@ -3,18 +3,28 @@ const log = require('../helpers/log');
 const { ERROR, SUCCESS } = require('../constans/log-types');
 const path = require('path');
 
+
 const replaceStringInFile = (filesToEdit, dir, toBeReplaced, replacement) => {
   filesToEdit.map(file => {
     const pathToFile = path.join(dir, file);
 
     try {
       const data = fs.readFileSync(pathToFile, 'utf8');
+      let oldData;
 
-      let oldData = JSON.stringify(data).replace(
-        new RegExp(toBeReplaced, 'g'),
-        replacement
-      );
-      let result = JSON.parse(oldData);
+      if (file == "package.json") {
+        oldData = JSON.stringify(data).replace(
+          new RegExp(`(?<=mdbreact-|\\\\"version\\\\": \\\\"|\\\\"mdbreact\\\": \\\\"|replace ')` + toBeReplaced, 'g'),
+          replacement
+        );
+      } else {
+        oldData = JSON.stringify(data).replace(
+          new RegExp(toBeReplaced, 'g'),
+          replacement
+        );
+      }
+
+      const result = JSON.parse(oldData);
 
       fs.writeFileSync(pathToFile, result, 'utf8');
 
